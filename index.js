4b6366fcb8762ce9e24050fddeec59a0b656b899
조재문 <moon@moon-mbp13m1.local>
2021-06-16 09:27:48 +0900

    로그인 및 토큰 생성

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const bodyParser = require('body-parser')
-const { User } = require("./models/User")
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require("./models/User");
 
-const config = require('./config/key')
+const config = require('./config/key');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -31,6 +33,42 @@ app.post('/register', (req, res) => {
         if (err) return res.json({success : false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if (!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "입력한 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다."
+                })
+            }
+            
+            //비밀번호까지 맞다면 토큰을 생성한다.
+            userInfo.generateToken( (err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // token을 저장한다.
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true,
+                    userId: user._id
+                })
+            })      
         })
     })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,50 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function( next ) {
+
+    var user = this;
+
+    if (user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRound, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, callbackFn) {
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return callbackFn(err)
+        callbackFn(null, isMatch)
+    })
+
+}
+
+userSchema.methods.generateToken = function (callbackFn) {
+
+    var user = this;
+
+    //jsonwebtoken을 이용해서 token을 생성한다.
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        if (err) return callbackFn(err)
+        callbackFn(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 

1fd46ee3d84dac5def44fcbfd2e6a30f66a4a32d
조재문 <moon@moon-mbp13m1.local>
2021-06-16 16:20:07 +0900

    인증

--- models/User.js
+++ models/User.js
@@ -41,25 +41,25 @@ userSchema.pre('save', function( next ) {
     if (user.isModified('password')) {
         // 비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRound, function (err, salt) {
-            if (err) return next(err)
+            if (err) return next(err);
 
             bcrypt.hash(user.password, salt, function (err, hash) {
-                if (err) return next(err)
-                user.password = hash
-                next()
-            })
-        })
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
     } else {
-        next()
+        next();
     }
 })
 
 userSchema.methods.comparePassword = function (plainPassword, callbackFn) {
 
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-        if (err) return callbackFn(err)
-        callbackFn(null, isMatch)
-    })
+        if (err) return callbackFn(err);
+        callbackFn(null, isMatch);
+    });
 
 }
 
@@ -69,15 +69,29 @@ userSchema.methods.generateToken = function (callbackFn) {
 
     //jsonwebtoken을 이용해서 token을 생성한다.
 
-    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
 
     user.token = token
     user.save(function(err, user) {
         if (err) return callbackFn(err)
         callbackFn(null, user)
-    })
+    });
+}
+
+userSchema.statics.findByToken = function (token, callbackFn) {
+    var user = this;
+    // 토큰을 Decode한다.
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        // 유저 ID를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 Token과 DB에 보관된 Token이 일치하는지 확인한다.
+
+        user.findOne({"_id" : decoded, "token" : token}, function (err, user) {
+            if (err) return callbackFn(err);
+            callbackFn(null, user);
+        });
+    });
 }
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = {User}
+module.exports = {User};
